34377e24cf7b7a6504bef12eb881e82345527ff2
jimeNestor <nestor.jimenez.003@gmail.com>
2023-09-07 15:39:33 -0500

    adding Stepper for Hours of Sleep

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -6,6 +6,11 @@
 // How many hours of sleep do they want?
 // How many cups of coffee do they drink per day?
 //
+// We will be using Core ML: to get a result telling us when they ought to be in bed
+// we will use the technique: regression analysis
+//
+//
+//
 //
 //  Created by Nestor Jimenez on 9/7/23.
 //
@@ -13,12 +18,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sleepAmount = 8.0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Stepper("\(sleepAmount.formatted()) hours of sleep", value: $sleepAmount, in: 4...12, step: 0.25)
+            
         }
         .padding()
     }
