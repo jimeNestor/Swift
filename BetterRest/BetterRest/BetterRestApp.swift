855bf28111c77863817089baa8f7f8b635713ce6
jimeNestor <nestor.jimenez.003@gmail.com>
2023-09-07 15:19:27 -0500

    adding the start of the project

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Nestor Jimenez on 9/7/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  BetterRest
+// App to help coffee drinkers get a good nights sleep by asking 3 questions
+// When do they want to wake up?
+// How many hours of sleep do they want?
+// How many cups of coffee do they drink per day?
+//
+//
+//  Created by Nestor Jimenez on 9/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
